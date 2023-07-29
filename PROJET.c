58c3dd0a8acc92fd07fabde9388af701cd76fca3
Modou Aicha Diop <modouaichadiop@gmail.com>
2023-07-29 14:51:51 +0000

    main update

--- PROJET.c
+++ PROJET.c
@@ -1,40 +1,35 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 #include "fonction.h"
-
 
 int main()
 {
-char choix;
-    
-    do{
+    char choix;
+
+    do
+    {
         switch (menu())
         {
-            case 1:
-                    liste_matiere();
-                    break;
-            case 2:
-                creer_etudiant();
-                break;
-            case 3:
-                liste_etudiant();
-                break;
-            case 4:
-                afficheBulletin();
-                break;
-            default:
-                printf("\n Choix indisponible \n");
-                break;
+        case 1:
+            liste_matiere();
+            break;
+        case 2:
+            creer_etudiant();
+            break;
+        case 3:
+            liste_etudiant();
+            break;
+        case 4:
+            afficheBulletin();
+            break;
+        default:
+            printf("\n Choix indisponible \n");
+            break;
         }
         printf("\nVoulez-vous retourner au Menu? (y = Oui) : ");
-        scanf("%c",&choix);
-        
-    }while(getchar()=='y');
-        
+        scanf("%c", &choix);
 
-        
+    } while (getchar() == 'y');
 
-
-return 0;
-    
+    return 0;
 }
